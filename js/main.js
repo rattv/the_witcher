5ee2474f16061129b638ce2b513a06d2210510ef
rattv <56552417+rattv@users.noreply.github.com>
2020-02-07 13:18:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.arrow',
+      },
+      breakpoints: {
+        540: {
+          slidesPerView: 2
+          
+        }
+      }
+    
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click',function () {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
